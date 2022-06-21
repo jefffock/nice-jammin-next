b280c839eb48e7daaca48726308064bbf91f8529
Jeff Fox <jeffaveryfox@gmail.com>
2022-06-20 23:34:29 -0500

    Filter list chips refresh when appropriate

--- components/FilterList.js
+++ components/FilterList.js
@@ -6,18 +6,26 @@ import Box from '@mui/material/Box';
 
 export default function FilterList({ artist, setArtist, tagsSelected, setTagsSelected, beforeDate, afterDate, setBeforeDate, setAfterDate, song, setSong }) {
 
+  useEffect(()=> {
+    console.log('artist', artist)
+    console.log('song', song)
+    console.log('tagsSelected', tagsSelected)
+    console.log('beforeDate', beforeDate)
+    console.log('afterDate', afterDate)
+  })
+
   return (
     //justifyContent="center"
     <Box display="flex" mx="auto" maxWidth='900px'>
       <Box sx={{ p:'0.5em' }}>
       {artist && artist !== 'All Bands' &&
-      <FilterChip artist={artist} setArtist={setArtist}></FilterChip>}
+      <FilterChip artist={artist} setArtist={setArtist} key={artist}></FilterChip>}
       {song &&
-      <FilterChip key={song} song={song} setSong={setSong}></FilterChip>}
+      <FilterChip song={song} setSong={setSong} key={song} ></FilterChip>}
       {afterDate &&
-      <FilterChip afterDate={afterDate} setAfterDate={setAfterDate}></FilterChip>}
+      <FilterChip afterDate={afterDate} setAfterDate={setAfterDate} key={afterDate}></FilterChip>}
       {beforeDate &&
-      <FilterChip beforeDate={beforeDate} setBeforeDate={setBeforeDate}></FilterChip>}
+      <FilterChip beforeDate={beforeDate} setBeforeDate={setBeforeDate} key={beforeDate}></FilterChip>}
       {tagsSelected &&
       tagsSelected.map((tag) => (
       <FilterChip key={tag} tag={tag} tagsSelected={tagsSelected} setTagsSelected={setTagsSelected}></FilterChip>
