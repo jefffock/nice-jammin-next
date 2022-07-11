7c41e6c035ea06b75ef190c49e6ee4c20ab4b83d
Jeff Fox <jeffaveryfox@gmail.com>
2022-07-11 17:49:20 -0500

    Changed welcome message to contain instructions

--- components/Welcome.js
+++ components/Welcome.js
@@ -5,7 +5,9 @@ export default function Welcome() {
 
   return (
     <Box my="3em" mx="auto" px="0.5em" width="96vw" maxWidth="fit-content">
-    <Typography variant="h4" textAlign="center">What would you like to hear?</Typography>
+    <Typography textAlign="center" fontSize="28px" my="1em">1. Use the filters below to change the list of jams</Typography>
+    <Typography textAlign="center" fontSize="28px" my="1em">2. Click a jam to reveal the Listen and Rate buttons</Typography>
+    <Typography textAlign="center" fontSize="28px" my="1em">3. Listen, dance, and enjoy!</Typography>
     </Box>
   )
 }
