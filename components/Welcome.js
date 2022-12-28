2d3fdd8f73251efaa05eb892761ec7453ca9a4aa
Jeff Fox <jeffaveryfox@gmail.com>
2022-12-28 10:41:51 -0600

    Updated welcome and gratitude sections

--- components/Welcome.js
+++ components/Welcome.js
@@ -1,13 +1,21 @@
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box'
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 export default function Welcome() {
-
   return (
     <Box my="3em" mx="auto" px="0.5em" width="96vw" maxWidth="fit-content">
-    <Typography textAlign="center" fontSize="22px" my="1em">1. Use the filters below to change the list of jams</Typography>
-    <Typography textAlign="center" fontSize="22px" my="1em">2. Click a jam to reveal the Listen and Rate buttons</Typography>
-    <Typography textAlign="center" fontSize="22px" my="1em">3. Listen, dance, and enjoy!</Typography>
+      <Typography textAlign="center" fontSize="22px" my="1em">
+        So many jams, so little time:
+        <br />
+        Let's make 🔥 easier to find!
+      </Typography>
+      <Typography textAlign="center" fontSize="18px" my="1em">
+        Click a row
+        <br />
+        Surrender to the flow
+        <br />
+        ❤️
+      </Typography>
     </Box>
-  )
+  );
 }
