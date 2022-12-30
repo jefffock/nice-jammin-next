ae9fa1eed8f3e498f1589a2c394f50e8f966fdbe
Jeff Fox <jeffaveryfox@gmail.com>
2022-12-29 21:39:19 -0600

    Removed label, set aria-hidden=true for arrow column headcell

--- components/JamsTableCollapsible.js
+++ components/JamsTableCollapsible.js
@@ -100,6 +100,7 @@ function JamsTableHead({ order, orderBy, onRequestSort, showRatings }) {
               align={headCell.center ? "center" : "left"}
               padding={headCell.disablePadding ? "none" : "normal"}
               sortDirection={orderBy === headCell.id ? order : false}
+              aria-hidden={headCell.label === "arrow"}
             >
               <TableSortLabel
                 active={orderBy === headCell.id}
@@ -110,7 +111,7 @@ function JamsTableHead({ order, orderBy, onRequestSort, showRatings }) {
                     : () => {}
                 }
               >
-                {headCell.label}
+                {headCell.label !== "arrow" ? headCell.label : ""}
                 {orderBy === headCell.id ? (
                   <Box component="span" sx={visuallyHidden} aria-hidden="true">
                     {order === "desc"
@@ -129,24 +130,20 @@ function JamsTableHead({ order, orderBy, onRequestSort, showRatings }) {
               align={headCell.center ? "center" : "left"}
               padding={headCell.disablePadding ? "none" : "normal"}
               sortDirection={orderBy === headCell.id ? order : false}
+              aria-hidden={headCell.label === "arrow"}
             >
               <TableSortLabel
                 active={orderBy === headCell.id}
                 direction={orderBy === headCell.id ? order : "asc"}
-                onClick={createSortHandler(headCell.id)}
+                onClick={
+                  headCell.id !== "arrow"
+                    ? createSortHandler(headCell.id)
+                    : () => {}
+                }
               >
-                {headCell.label === "Rating" ? (
-                  <>
-                    <StarRateIcon />
-                    <p>Rating</p>
-                  </>
-                ) : headCell.label === "Date" ? (
-                  <CalendarMonthIcon aria-label="sort by date button" />
-                ) : (
-                  headCell.label
-                )}
+                {headCell.label !== "arrow" ? headCell.label : ""}
                 {orderBy === headCell.id ? (
-                  <Box component="span" sx={visuallyHidden} aria-hidden="true">
+                  <Box component="span" sx={visuallyHidden}>
                     {order === "desc"
                       ? "sorted descending"
                       : "sorted ascending"}
