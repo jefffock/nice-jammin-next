0e6a11cbaa09b55fd6d8f7778bf08cedd8ae3a05
Jeff Fox <jeffaveryfox@gmail.com>
2023-01-01 00:47:06 -0600

    Changed styling of "View your Contributions" link

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,8 @@
 import Typography from '@mui/material/Typography';
 import ReportIssue from './ReportIssue';
 import Stack from '@mui/material/Stack';
-import { Button } from '@mui/material/Button';
-import Link from 'next/link'
-
+import Button from '@mui/material/Button';
+import Link from 'next/link';
 
 export default function Footer({ profile, user }) {
 	return (
@@ -12,12 +11,14 @@ export default function Footer({ profile, user }) {
 			sx={{ m: '2em', display: 'flex', alignItems: 'center' }}
 		>
 			{profile && profile.points && (
-        <>
-				<Typography>
-					{profile.name}&nbsp;-&nbsp;{profile.points} points. Thank you!
-				</Typography>
-        <Link href={`/fans/${profile.name}`}>View your contributions</Link>
-        </>
+				<>
+					<Typography>
+						{profile.name}&nbsp;-&nbsp;{profile.points} points. Thank you!
+					</Typography>
+					<Link href={`/fans/${profile.name}`}>
+						<Button sx={{textTransform: 'none'}}>View your contributions</Button>
+					</Link>
+				</>
 			)}
 			<ReportIssue
 				user={user}
